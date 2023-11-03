b0ce2959e1b101cfa85a1239ff9c8e81ca4827c4
binti khusnul kotimah <145413194+bhintii@users.noreply.github.com>
2023-11-03 16:52:06 +0700

    Add files via upload

---
+++ pertemuan_5/MobilMatic.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pertemuan_5;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class MobilMatic extends mobil {
+    
+    @Override
+    public void jalankan(){
+        System.out.println("Gigi di posisi D, tekan gas");
+    }
+    
+}
+
+   
