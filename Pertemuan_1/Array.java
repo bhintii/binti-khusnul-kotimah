b64d287ddfa9d87c53226ba5266e04c725815fde
binti khusnul kotimah <145413194+bhintii@users.noreply.github.com>
2023-11-03 10:33:17 +0700

    Add files via upload

---
+++ Pertemuan_1/Array.java
@@ -0,0 +1,35 @@
+package Pertemuan_1;
+import java.util.Scanner;
+public class Array {
+public static void main(String[] args) {
+
+   System.out.println("Mengakses Data Array:");
+    String[] Hari = null;
+   System.out.println("Hari ke 1: " + Hari[0]);
+   System.out.println("Hari ke 2: " + Hari[1]);
+   System.out.println("Hari ke 3: " + Hari[2]);
+   System.out.println("Hari ke 4: " + Hari[3]);
+   System.out.println("Hari ke 5: " + Hari[4]);
+   
+    Scanner input=new Scanner(System.in);
+   
+        System.out.print("Masukan Jumlah Kata : ");
+        int n=input.nextInt();
+        
+        //Deklarasi array
+        String kata[]=new String[n];
+      
+        //Input data ke array
+        for(int i=0;i<kata.length;i++)
+        {
+           System.out.print("Kata ke "+(i+1)+" : ");
+           kata [i]=input.next();
+        }
+        //Menampilkan data dalam array
+        System.out.println("Kata-kata yang dimasukan");
+    for (String kata1 : kata) {
+        System.out.println(kata1);
+    }
+    }
+    
+}
---
+++ Pertemuan_1/TipeData.java
@@ -0,0 +1,22 @@
+package Pertemuan_1;
+
+public class TipeData {
+    public static void main(String[] args) {
+      String nama = "Bintii";
+        System.out.println("Nama : " + nama);
+      int umur = 20;
+        System.out.println("Umur : " + umur);
+      float berat = (float) 42.5;
+        System.out.println("Berat Badan : " + berat);
+      double panjang = 147.5;
+        System.out.println("Tinggi Badan : " + panjang);
+      char gender = 'P';
+        System.out.println("Jenis Kelamin = " + gender);
+      boolean aktif = true;
+        System.out.println("Mahasiswa Aktif : " + aktif);
+      
+   
+     
+    }
+    
+}
