c016290f0b585fc8a5803843d8d6765f4e42356a
binti khusnul kotimah <145413194+bhintii@users.noreply.github.com>
2023-11-08 13:12:39 +0700

    Add files via upload

---
+++ pertemuan_4/BangunDatar.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pertemuan_4;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class BangunDatar {
+    
+    float luas(){
+        System.out.println("Menghitung luas Bangun Datar");
+        
+        return 0;
+    
+}
+    float keliling(){
+        System.out.println("Menghitung keliling Bangun Datar");
+        return 0;
+    }
+    
+}
