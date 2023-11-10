f2fda9762b25280cdfcf2a533aec5d78b072272c
binti khusnul kotimah <145413194+bhintii@users.noreply.github.com>
2023-11-10 09:01:05 +0700

    Add files via upload

---
+++ pertemuan_7polimorfismeDinamis/BangunDatar.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pertemuan_7polimorfismeDinamis;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class BangunDatar {
+    float luas(){
+        System.out.println("Menghitung Luas Bangun Datar");
+        return 0;
+    }
+    float Keliling(){
+        System.out.println("Menghitung Keliling Bangun Datar");
+        return 0;
+    }
+    public static void main(String[] args) {
+        BangunDatar bd = new BangunDatar();
+        bd.Keliling();
+    }
+}
